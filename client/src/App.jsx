efee6e972eb4a54dd48cf8047d64ba73fc983de1
himanshu <hmxnsu.blvck@gmail.com>
2023-03-03 00:23:08 +0530

    ...

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>heyyy</div>
+  )
+}
+
+export default App
---
+++ client/src/main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import App from "./App";
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <ThirdwebProvider chainId={ChainId.Goerli}>
+    <Router>
+      <App />
+    </Router>
+  </ThirdwebProvider>
+);
