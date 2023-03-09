b94461080b9c1f3aca1c37987fac1b1ccc1ab83c
himanshu <hmxnsu.blvck@gmail.com>
2023-03-09 23:56:34 +0530

    avjiogasdkflsdo

--- client/src/pages/CampaignDetails.jsx
+++ client/src/pages/CampaignDetails.jsx
@@ -1,4 +1,13 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import {
+  useLocation,
+  useNavigate
+} from 'react-router-dom'
+import { useStateContext } from '../context'
+import { CountBox, CustomBtn, Loader } from '../components'
+import { ethers } from 'ethers'
+import {calculateBarPercentage} from '../utils'
+import { thirdweb } from '../assets'
 
 const CampaignDetails = () => {
   return (
