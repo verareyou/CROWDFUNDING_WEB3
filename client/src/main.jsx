9ec3f7008a76ea2a2ef6bd8ae9f5473007acd62c
himanshu <hmxnsu.blvck@gmail.com>
2023-03-23 00:40:53 +0530

    ...

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,7 +8,7 @@ import { StateContextProvider } from "./context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <ThirdwebProvider chainId={ChainId.Localhost}>
+  <ThirdwebProvider chainId={ChainId.Goerli}>
     <StateContextProvider>
       <Router>
         <App />
